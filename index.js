1429b40da861f099bc1cfb37d6a844c8322df41d
PaoloBSK <0219468@up.edu.mx>
2022-09-19 20:12:01 -0500

    update

--- index.js
+++ index.js
@@ -3,14 +3,11 @@ const formulario = document.querySelector('#formulario')
 function limpiar(){
     document.getElementById('formulario').reset();
 }
+function enviar(nombre1,nombre2,nombre3){
+    console.log(nombre1,nombre2,nombre3)
 
+}
 
 //formulario.addEventListener("submit", validar)
-function enviar(e){
-    e.preventDefault();
-    const nombres = document.querySelector("#nombre").value
-    const apellidos = document.querySelector("#apellidos").value
-    const apodo = document.querySelector("#apodo").value
-    console.log(nombres,apellidos,apodo)
-}
+
 
